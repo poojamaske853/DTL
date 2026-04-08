53e0c85bee18af9435806b15294965bdaaf8550d
nandini535 <kharchenandini@gmail.com>
2026-04-08 22:42:35 +0530

    Updated mul.c

--- mul.c
+++ mul.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+int multiply(int x, int y) {
+    return x * y;
+}
+
 int main() {
     int a, b, result;
 
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    result = a * b;
+    result = multiply(a, b);
 
     printf("Multiplication = %d\n", result);
 
